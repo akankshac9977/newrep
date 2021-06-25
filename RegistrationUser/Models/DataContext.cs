356fe3fd169b3b49a55beb4bea7346079d98bc45
akanksha choubey <akankshac9977@gmail.com>
2021-06-25 09:43:22 +0530

    first

---
+++ RegistrationUser/Models/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationUser.Models
+{
+    public class DataContext : DbContext
+    {
+        public virtual DbSet<Users> users { get; set; }
+        public DataContext() : base("MyConnectionString")
+        {
+
+        }
+    }
+}
---
+++ RegistrationUser/Models/Users.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationUser.Models
+{
+    public class Users
+    {
+        [Key]
+        public int User { get; set; }
+        [Display(Name = "First Name")]
+        [Required]
+       
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required]
+        public string LastName { get; set; }
+        //public string Address { get; set; }
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        [Required]
+        public string Email { get; set; }
+        [Display(Name = "Mobile ")]
+        [Required]
+        public string Mobile { get; set; }
+        [Display(Name = "Username")]
+        [Required]
+        public string Username { get; set; }
+    }
+}
